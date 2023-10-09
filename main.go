c35df57b67bf1b61fab21f3561f205126e84c876
Sam Westmoreland <samwestmoreland@gmail.com>
2023-10-09 21:09:53 +0100

    Loop through the csv file

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 )
@@ -25,5 +27,41 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Println("File Upload Endpoint Hit")
+	// Parse the multipart form
+	err := r.ParseMultipartForm(10 << 20) // 10MB
+	if err != nil {
+		http.Error(w, "Failed to parse form", http.StatusInternalServerError)
+		return
+	}
+
+	// Get the file from the form data
+	file, _, err := r.FormFile("file")
+	if err != nil {
+		http.Error(w, "Failed to get file", http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	// Create a new CSV reader
+	reader := csv.NewReader(file)
+
+	// Read the CSV data line by line
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			http.Error(w, "Failed to read CSV data", http.StatusInternalServerError)
+			return
+		}
+
+		// Process the CSV record
+		// Here we'll store the record into a database
+
+		// Example: Print the CSV record
+		fmt.Println(record)
+	}
+
+	fmt.Fprintln(w, "File uploaded and processed successfully!")
 }
